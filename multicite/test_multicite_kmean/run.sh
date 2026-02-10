9adb740d60f2b522d4b4753a9d4516805d5fc249
anonymous-2026-ai <anonymous2027ai@gmail.com>
2026-02-10 10:27:10 +0900

    refactor code

--- multicite/test_multicite_kmean/run.sh
+++ multicite/test_multicite_kmean/run.sh
@@ -14,7 +14,7 @@ do
         --data_dir ../../datasets/data_multicite/ \
         --max_seq_length 512 --per_gpu_train_batch_size 1 \
         --learning_rate ${LEARNING_RATE} --num_train_epochs 5 \
-        --output_dir result_baseline --seed 100 \
+        --output_dir result_baseline --seed 0 \
         --classification_type multilabel --overwrite_cache \
         --overwrite_output_dir --gradient_accumulation_steps ${bs} \
          --save_steps 500 --k 0 --logging_steps 500 --evaluate_during_training --n_iter_sent ${n_sent} --n_iter_word ${n_word}
